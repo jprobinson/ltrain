ed6ac35b058a8105593567b73ddb5fd84a2f6d1d
JP Robinson <john.paul.robinson@gmail.com>
2014-09-01 16:00:19 -0400

    latest n greatest

--- js/main.js
+++ js/main.js
@@ -1,32 +1,72 @@
+var first = true;
+
 function getTrainTime(callback) {
-    console.log('getting train time');
-    $.get('/svc/subway-api/v1/next-trains/L11',
+    var stop = $('#stop').val();
+    console.log('getting train time for '+stop);
+    $.get('/svc/subway-api/v1/next-trains/'+stop,
         function(data) {
             console.log(data);
-            var train = new Date(data.northbound);
-            callback(train);
+            var next = new Date(data.southbound[0]);
+            var following = new Date(data.southbound[1]);
+
+            var brooklyn = $('.toggle').data('toggles');
+            if (brooklyn.active) {
+                next = new Date(data.northbound[0]);
+                following = new Date(data.northbound[1]);
+            } 
+            callback(next, following);
         });
 }
 
-function updateClock(time) {
-    $('#clock').countdown(time, function(event) {
-        var format = '%M:%S';
-        $(this).html(event.strftime(format));
-    }).on('finish', function() {
-        setTimeout(function() {
-            getTrainTime(updateClock);
-        }, 500);
-    });
+function timeoutTrain() {
+    getTrainTime(updateClock);
+    setTimeout(function() {
+            timeoutTrain();
+    }, 20000);
+}
+
+function updateClock(next, following) {
+    if (!first) {
+        $('#nextClock').countdown(next);
+        $('#followClock').countdown(following);
+
+    } else {
+        first = false;
+        $('.clocks').each(function(){
+            var $this = $(this), id = $(this).attr('id');  
+            var time = following;
+            if (id == 'nextClock') {
+                time = next;
+            }
+            $this.countdown(time, function(event) {
+                if (event.type == "finish.countdown") {
+                   console.log('finished!?'); 
+                }
+                var format = '%M:%S';
+                $(this).html(event.strftime(format));
+            });
+        });
+    }
 }
 
 $(function() {
-    getTrainTime(updateClock);
-
-    setTimeout(function() {
+    $('.toggle').toggles({
+        text:{on:'Manhattan',off:'Brooklyn'},
+        on: true,
+        width:250,
+        height:50
+    });
+    $('.toggle').on('toggle', function(){
         getTrainTime(updateClock);
-        setTimeout(function() {
-            getTrainTime(updateClock);
-        }, 60000);
-    }, 60000);
-
+    });
+    var stop = $('#stop');
+    stop.change(function(event){
+        getTrainTime(updateClock);
+    });
+    stop.select2({width:"element"});
+    
+    getTrainTime(updateClock);
+    setTimeout(function() {
+       timeoutTrain(); 
+    }, 20000);
 });
